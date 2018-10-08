6f8d5a9d55964f1fc44aec2017b94c7f2b6fd089
iclementine <ijasmine@yeah.net>
2018-10-08 21:45:52 +0800

    add __init__.py and necessities

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .parser_state import State
+from .sentence import ConllSent
+from .transition_system import TransitionSystemBase, ArcStandard, ArcHybrid, ArcEagerReduce, ArcEagerShift
+
+__version__ = '0.1.0'
+
+__all__ = ['sentence',
+           'parser_state',
+           'transition_system']
