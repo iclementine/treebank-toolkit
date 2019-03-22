fdae49f21fce86f964a0232c84732baea12f498d
iclementine <ijasmine@yeah.net>
2019-03-22 08:48:32 +0800

    add dynamic oracle for arc-standard

--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -1,16 +1,21 @@
 import conllu
 import treebank_toolkit as tbtk
-from treebank_toolkit.dynamic import ArcEager, ArcHybrid
+from treebank_toolkit.dynamic import ArcEager, ArcHybrid, ArcStandard
 import random
 
-eager = ArcEager()
-f = open("/home/clementine/projects/treebanks/ctb51_zpar/dev.conll", 'rt')
+f = open("/home/clementine/projects/treebanks/ctb51_zpar/train.conll", 'rt')
 sentences = [tbtk.ConllSent.from_conllu(sent) for sent in conllu.parse(f.read())]
+
+
+# gold tree recovery test, though ambiguity in transitions, the gold tree constructed is the same
+eager = ArcEager()
 for sent in sentences:
 	state = tbtk.State.init_from_sent(sent)
 	while not state.is_final():
+		valid = eager._valid_transitions(state)
 		oracle = eager.dynamic_oracle(state)
-		#print(oracle)
+		if (len(oracle) > 1):
+			print("Eager::oracle: ", oracle)
 		g = random.choice(oracle)
 		state = eager.step(state, g)
 	assert([state.arcs[x][0] for x in range(1, len(sent) + 1)] == sent.head[1:])
@@ -21,10 +26,73 @@ hybrid = ArcHybrid()
 for sent in sentences:
 	state = tbtk.State.init_from_sent(sent)
 	while not state.is_final():
+		valid = hybrid._valid_transitions(state)
 		oracle = hybrid.dynamic_oracle(state)
-		#print(oracle)
+		if (len(oracle) > 1):
+			print("Hybrid::oracle: ", oracle)
 		g = random.choice(oracle)
 		state = hybrid.step(state, g)
 	assert([state.arcs[x][0] for x in range(1, len(sent) + 1)] == sent.head[1:])
 	#print([state.arcs[x][0] for x in range(1, len(sent) + 1)])
 	#print(sent.head[1:])
+
+	
+std = ArcStandard()
+for sent in sentences:
+	state = tbtk.State.init_from_sent(sent)
+	while not state.is_final():
+		#print(state.sent.children)
+		#print(state.stack, list(reversed(state.buf)))
+		valid = std._valid_transitions(state)
+		oracle = std.dynamic_oracle(state)
+		if (len(oracle) > 1):
+			print("Standard::oracle: ", oracle)
+		g = random.choice(oracle)
+		state = std.step(state, g)
+	assert([state.arcs[x][0] for x in range(1, len(sent) + 1)] == sent.head[1:])
+	#print([state.arcs[x][0] for x in range(1, len(sent) + 1)])
+	#print(sent.head[1:])
+
+# when not in gold path, may be there is more than one oracle
+# when deviated from the gold path, dynamic oracle is still available to guide the parser
+# to generate a sub-optimal tree
+eager = ArcEager()
+for sent in sentences:
+	state = tbtk.State.init_from_sent(sent)
+	while not state.is_final():
+		valid = eager._valid_transitions(state)
+		oracle = eager.dynamic_oracle(state)
+		if (len(oracle) > 1):
+			print("Eager::oracle: ", oracle)
+		g = random.choice(valid)
+		state = eager.step(state, g)
+	#print([state.arcs[x][0] for x in range(1, len(sent) + 1)])
+	#print(sent.head[1:])
+
+hybrid = ArcHybrid()
+for sent in sentences:
+	state = tbtk.State.init_from_sent(sent)
+	while not state.is_final():
+		valid = hybrid._valid_transitions(state)
+		oracle = hybrid.dynamic_oracle(state)
+		if (len(oracle) > 1):
+			print("Hybrid::oracle: ", oracle)
+		g = random.choice(valid)
+		state = hybrid.step(state, g)
+	#print([state.arcs[x][0] for x in range(1, len(sent) + 1)])
+	#print(sent.head[1:])
+	
+std = ArcStandard() 
+for sent in sentences:
+	state = tbtk.State.init_from_sent(sent)
+	while not state.is_final():
+		valid = std._valid_transitions(state)
+		oracle = std.dynamic_oracle(state)
+		if (len(oracle) > 1):
+			print("Standard::oracle: ", oracle)
+		g = random.choice(valid)
+		state = std.step(state, g)
+	#print([state.arcs[x][0] for x in range(1, len(sent) + 1)])
+	#print(sent.head[1:])
+
+
--- treebank_toolkit/dynamic.py
+++ treebank_toolkit/dynamic.py
@@ -302,3 +302,157 @@ class ArcEager(TransitionSystemBase):
         elif control == "backbone":
             tsn, lbl = string, "_"
         return (tsn, lbl)
+
+    
+class ArcStandard(TransitionSystemBase):
+    _actions_list = ["Shift", "LeftReduce", "RightReduce"]
+    
+    @classmethod
+    def _valid_transitions(cls, parser_state):
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        
+        stack, buf = parser_state.stack, parser_state.buf
+        
+        valid_transitions = []
+        
+        if len(buf) > 0:
+            valid_transitions.append(SHIFT)
+        if len(stack) > 2:
+            valid_transitions.extend([LEFT, RIGHT])
+        elif len(stack) == 2 and len(buf) == 0:
+            valid_transitions = [RIGHT]
+        
+        return valid_transitions
+    
+    @classmethod
+    def step(cls, parser_state, action):
+        """
+        action is a tuple of str 
+        """
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        if isinstance(action, tuple):
+            tsn, lbl = action
+        else:
+            tsn, lbl = action, "_"
+        
+        state = State.copy(parser_state)
+        stack = state.stack
+        buf = state.buf
+        tags = state.tags
+        arcs = state.arcs
+        
+        cand = cls._valid_transitions(state)
+        assert tsn in cand
+        
+        if tsn == SHIFT:
+            tags[buf[-1]] = lbl
+            stack.append(buf.pop())
+            if len(buf) == 0:
+                state.seen_the_end = True
+        elif tsn == LEFT:
+            arcs[stack[-2]] = (stack[-1],  lbl) 
+            stack.pop(-2)
+        elif tsn == RIGHT:
+            arcs[stack[-1]] = (stack[-2], lbl)
+            stack.pop()
+        
+        return state
+    
+    @classmethod
+    def gold_action(cls, parser_state):
+        """
+        derive next gold action from reference, which in included in the parser_state.
+        """
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        
+        stack = parser_state.stack
+        buf = parser_state.buf
+        upos = parser_state.sent.upos
+        heads = parser_state.sent.head
+        deprels = parser_state.sent.deprel
+        stack_top_children = parser_state.sent.children[stack[-1]]
+        
+        stack_top_done = True
+        for x in buf:
+            if x in stack_top_children:
+                stack_top_done = False
+                break
+        
+        if len(stack) > 2 and heads[stack[-2]] == stack[-1]:
+            lbl = deprels[stack[-2]]
+            tsn = LEFT
+        elif len(stack) > 1 and heads[stack[-1]] == stack[-2] and stack_top_done:
+            lbl = deprels[stack[-1]]
+            tsn = RIGHT
+        else:
+            lbl = upos[buf[-1]]
+            tsn = SHIFT
+        
+        return (tsn, lbl)
+    
+    @classmethod
+    def dynamic_oracle(cls, parser_state):
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        valid = cls._valid_transitions(parser_state)
+        oracle = []
+        
+        # state
+        stack = parser_state.stack
+        buf = parser_state.buf
+        # reference
+        upos = parser_state.sent.upos
+        heads = parser_state.sent.head
+        children = parser_state.sent.children
+        deprels = parser_state.sent.deprel
+        
+        if SHIFT in valid:
+            buffer_top_head = heads[buf[-1]]
+            stack_top_head = heads[stack[-1]]
+            stack_top_children = children[stack[-1]]
+            # 1. if there is no arc between s0 and b0, and S0 has collected all its right children [then there is no chance to expose S0 on the top of the stack, so no chance to reduce it]
+            # 2. if s0 has a right head(in the buffer) and s0 has not collected all its left children [then s0's head won't be reduced, there is no chance for s0 to reduce to its head cause it cannot be exposed on the top of the stack to collect its left children]
+            # then shift is not an dynamic oracle
+            if not ((buffer_top_head != stack[-1] and stack_top_head != buf[-1] and stack_top_head < stack[-1] and (not any(x in buf for x in stack_top_children))) or (stack_top_head > stack[-1] and any([x in stack for x in stack_top_children]))):
+                oracle.append(SHIFT)
+            
+        if RIGHT in valid:
+            stack_top_head = heads[stack[-1]]
+            stack_top_children = children[stack[-1]]
+            if not ((stack_top_head != stack[-2]) or any(x in buf for x in stack_top_children)):
+                oracle.append(RIGHT)
+        
+        if LEFT in valid:
+            stack_2_head = heads[stack[-2]]
+            stack_2_children = children[stack[-2]]
+            if not ((stack_2_head != stack[-1]) or any(x in stack for x in stack_2_children)):
+                oracle.append(LEFT)
+        return oracle
+        
+    @classmethod
+    def action_to_str(cls, action, control="normal"):
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        
+        if isinstance(action, tuple):
+            tsn, lbl = action
+        else:
+            tsn, lbl = action, '_'
+        
+        if control == "normal":
+            if tsn == SHIFT:
+                return tsn
+            else:
+                return "{}-{}".format(tsn, lbl)
+        elif control == "backbone":
+            return tsn
+    
+    @classmethod
+    def str_to_action(cls, string, control="normal"):
+        SHIFT, LEFT, RIGHT = cls._actions_list
+        if control == "normal":
+            if string.startswith(SHIFT):
+                tsn, lbl = string, "_"
+            else:
+                tsn, lbl = string.split("-", 1)
+        elif control == "backbone":
+            tsn, lbl = string, "_"
+        return (tsn, lbl)
